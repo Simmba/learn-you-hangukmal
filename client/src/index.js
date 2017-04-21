2a6ef66a8b613601eeef392c6b2aa20d0eda1cf8
Simmba <simmern.kang@gmail.com>
2017-04-20 21:31:21 -0700

    all changes

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'; //equivalent to var some = require('some')
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
